51a070c25f9731c28bc1ad23392069c6b79e1572
unclepete-20 <63658079+unclepete-20@users.noreply.github.com>
2022-11-30 23:59:44 -0600

    Se ha finalizado el proyecto 1

--- utils.py
+++ utils.py
@@ -1,18 +1,20 @@
-"""
-Universidad del Valle de Guatemala
-(CC2018) Gráficas por Computadora
-Librería de archivos .bmp
-Santiago Taracena Puga (20017)
-"""
-
-# Módulos necesarios.
 import struct
 
-# Funciones lambda extra utilizadas.
-color = lambda r, g, b: bytes([b, g, r])
-char = lambda character: struct.pack("=c", character.encode("ascii"))
-word = lambda word: struct.pack("=h", word)
-dword = lambda dword: struct.pack("=l", dword)
+def char(c):
+    # 1 byte
+    return struct.pack('=c',c.encode('ascii'))
+
+def word(w):
+    # 2  bytes
+    return struct.pack('=h',w)
+
+def dword(d):
+    #4 bytes
+    return struct.pack('=l', d)
+
+def color(r, g, b):
+  return bytes([round(b),round(g),round(r)])
+
 unpack = lambda buffer: struct.unpack("=l", buffer)[0]
 
 # Función que convierte coordenadas absolutas a relativas.
@@ -21,7 +23,7 @@ def absolute_to_relative_conversion(x, y, width, height):
   px, py = ((x - cx) / cx), ((y - cy) / cy)
   return px, py
 
-# Constantes extra utilizadas.
+
 BLACK = color(0, 0, 0)
 WHITE = color(255, 255, 255)
 RED = color(255, 0, 0)
